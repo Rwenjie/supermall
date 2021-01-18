b577f62c3066853dceb785c3f02e634a3ad87da9
Rwenjie <1621317578@qq.com>
2021-01-18 21:14:13 +0800

    首页开发

--- src/components/content/mainTabBar/MainTabBar.vue
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -1,29 +1,27 @@
 <template>
   <tab-bar>
-
-    <tab-bar-item path="/home" activeColer="#ff2f82">
-      <img slot="item-icon" src="assets/img/tabbar/shouye.svg" alt="">
-      <img slot="item-icon-active" src="assets/img/tabbar/shouye_active.svg" alt="">
+    <tab-bar-item path="/home">
+      <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
       <div slot="item-text">首页</div>
     </tab-bar-item>
     <tab-bar-item path="/category">
-      <img slot="item-icon" src="assets/img/tabbar/fenlei.svg" alt="">
-      <img slot="item-icon-active" src="assets/img/tabbar/fenlei_active.svg" alt="">
+      <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
       <div slot="item-text">分类</div>
     </tab-bar-item>
-    <tab-bar-item path="/shopcart">
-      <img slot="item-icon" src="assets/img/tabbar/gouwu.svg" alt="">
-      <img slot="item-icon-active" src="assets/img/tabbar/gouwu_active.svg" alt="">
+    <tab-bar-item path="/cart">
+      <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg" alt="">
       <div slot="item-text">购物车</div>
     </tab-bar-item>
     <tab-bar-item path="/profile">
-      <img slot="item-icon" src="assets/img/tabbar/wode.svg" alt="">
-      <img slot="item-icon-active" src="assets/img/tabbar/wode_active.svg" alt="">
+      <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
       <div slot="item-text">我的</div>
     </tab-bar-item>
   </tab-bar>
 </template>
-
 <script>
 
   import TabBar from "components/common/tabbar/TabBar";
